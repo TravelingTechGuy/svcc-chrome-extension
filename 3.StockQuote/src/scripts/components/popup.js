a40bf955283f64a616c391b2c289d64fb645d420
Guy Vider <guy@guyvider.com>
2017-06-29 18:08:46 -0700

    present quotes

--- 3.StockQuote/src/scripts/components/popup.js
+++ 3.StockQuote/src/scripts/components/popup.js
@@ -3,36 +3,69 @@ import Footer from './footer';
 import './popup.css';
 
 export default class Popup extends React.Component {
-  showBadge(badgeText = '', badgeColor = []) {
-    chrome.browserAction.setBadgeBackgroundColor({color: badgeColor});
-    chrome.browserAction.setTitle({title: badgeText});
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [],
+      date: 'Updating...'
+    };
+    chrome.runtime.sendMessage({action: 'getQuotes'}, function(response) {
+      this.setState(response);
+    }.bind(this));
+  }
+
+  showBadge(badgeText = '', badgeTitle = '', badgeColor = []) {
+    chrome.browserAction.setTitle({title: badgeTitle});
     chrome.browserAction.setBadgeText({text: badgeText});
+    if(badgeColor.length) {
+      chrome.browserAction.setBadgeBackgroundColor({color: badgeColor});
+    }
   }
 
-  showSuccessBadge() {
-    this.showBadge('Yay!', [0, 149, 72, 230]);
+  showSymbols() {
+    if(this.state.quotes.length) {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>Symbol</th>
+              <th>Current</th>
+              <th>Change</th>
+              <th>Percent</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            this.state.quotes.map(quote => {
+              return (
+                <tr key={quote.symbol}>
+                  <td>{quote.symbol}</td>
+                  <td className="price">{quote.current}</td>
+                  <td className={'change ' + quote.change.startsWith('-') ? 'minus': 'plus'}>{quote.change}</td>
+                  <td className={'change pct ' + quote.changePct.startsWith('-') ? 'minus': 'plus'}>{quote.changePct}</td>
+                </tr>
+              );
+            })
+          }
+          </tbody>
+        </table>
+      );
+    }
+    else {
+      return <div>Please add symbols in the Options page</div>;
+    }
   }
-
-  showFailureBadge() {
-    this.showBadge('No!!!', [214, 39, 40, 230]);
-  }
-
-  hideBadge() {
-    showBadge();
-  }
-
   render() {
     return (
       <div className="container">
         <div className="header">
-          <img src="../icons/icon19.png" />
+          <img src="../icons/svcc.png" width="19" height="19" />
           <span>&nbsp;StockQuote Extension</span>&nbsp;
         </div>
         <div className="content">
-          <p>Here be list</p>
-          <button onClick={this.showSuccessBadge}>Success badge</button>
-          <button onClick={this.showFailureBadge}>Failure badge</button>
-          <button onClick={this.hideBadge}>Hide badge</button>
+          {this.showSymbols()}
+          <p/>
+          <div className="date">As of {this.state.date}</div>
         </div>
         <Footer size="small" />
       </div>
